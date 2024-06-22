67d00f618ec285aad8903cea4e88887353a6ac39
Daniil Nikulin <d.nikulin@innopolis.university>
2024-06-22 15:32:34 +0300

    feat: register form

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    import RegisterForm from './register-form.svelte'
+    import Switch from "$lib/components/ui/switch/switch.svelte";
+    import Button from "$lib/components/ui/button/button.svelte";
+    export let data;
+</script>
+
+<div class="main">
+    <header>
+        <span class="header"><a href="/">LinkLink In</a></span>
+        <div class="controls">
+            <Switch/>
+            <Button href="/login">Log in</Button>
+        </div>
+    </header>
+    <div class="form-wrapper">
+        <RegisterForm data={data.form}/>
+    </div>
+</div>
+
+<style>
+    .form-wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100vh;
+    }
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem 2rem;
+    }
+    .controls {
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+    }
+    .header {
+        font-weight: 700;
+        font-size: 1.5rem;
+    }
+</style>
---
+++ src/routes/register/register-form.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    import * as Form from "$lib/components/ui/form";
+    import { Input } from "$lib/components/ui/input";
+    import { formSchema, type FormSchema } from "./schema";
+    import {
+        type SuperValidated,
+        type Infer,
+        superForm,
+    } from "sveltekit-superforms";
+    import { zodClient } from "sveltekit-superforms/adapters";
+
+    export let data: SuperValidated<Infer<FormSchema>>;
+
+    const form = superForm(data, {
+        validators: zodClient(formSchema),
+    });
+
+    const { form: formData, enhance } = form;
+</script>
+
+<form method="POST" use:enhance>
+    <h1>Register</h1>
+    <div>
+        <Form.Field {form} name="username">
+            <Form.Control let:attrs>
+                <Form.Label>Username</Form.Label>
+                <Input {...attrs} placeholder="username" bind:value={$formData.username} />
+
+            </Form.Control>
+            <Form.FieldErrors />
+        </Form.Field>
+        <Form.Field {form} name="email">
+            <Form.Control let:attrs>
+                <Form.Label>Email</Form.Label>
+                <Input {...attrs} placeholder="username@example.com" bind:value={$formData.email} />
+
+            </Form.Control>
+            <Form.FieldErrors />
+        </Form.Field>
+        <Form.Field {form} name="password">
+            <Form.Control let:attrs>
+                <Form.Label>Password</Form.Label>
+                <Input {...attrs} placeholder="••••••••••••" type="password" bind:value={$formData.password} />
+            </Form.Control>
+            <Form.FieldErrors />
+        </Form.Field>
+        <Form.Field {form} name="password_repeat">
+            <Form.Control let:attrs>
+                <Form.Label>Repeat your password</Form.Label>
+                <Input {...attrs} placeholder="••••••••••••" type="password" bind:value={$formData.password_repeat} />
+            </Form.Control>
+            <Form.FieldErrors />
+        </Form.Field>
+    </div>
+    <Form.Button class="w-full">Submit</Form.Button>
+    <p>Already have an account? <a href="/login">Log in</a></p>
+</form>
+
+<style>
+    h1 {
+        font-weight: 600;
+        font-size: 2rem;
+    }
+    a {
+        text-decoration: underline;
+    }
+    form {
+        width: 22rem;
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: 0.25rem;
+    }
+</style>
