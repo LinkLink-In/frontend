2001576e01836e1666affb1f9131cc2fd6e181df
Daniil Nikulin <d.nikulin@innopolis.university>
2024-06-30 15:24:38 +0300

    chore: change custom message

--- src/routes/login/schema.ts
+++ src/routes/login/schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    email: z.string().email(),
+    email: z.string().email('This email is invalid'),
     password: z.string()
 });
 
