19e45f96d7f3f0a87b389f0577da6148aaeed686
Daniil Nikulin <d.nikulin@innopolis.university>
2024-06-30 16:10:52 +0300

    chore: handle registration submission

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,7 +1,8 @@
-import { fail } from "@sveltejs/kit";
-import { superValidate } from "sveltekit-superforms";
+import {fail, redirect} from "@sveltejs/kit";
+import {setError, superValidate} from "sveltekit-superforms";
 import { formSchema } from "./schema";
 import { zod } from "sveltekit-superforms/adapters";
+import {type UserRead, userRegister} from "$lib/api/auth";
 
 export const load = async () => {
     return {
@@ -17,6 +18,21 @@ export const actions = {
                 form,
             });
         }
+        const registerResponse: UserRead = await userRegister(form.data.name, form.data.email, form.data.password);
+        if (!registerResponse.ok) {
+            switch (registerResponse.detail) {
+                case "REGISTER_USER_ALREADY_EXISTS":
+                    return setError(form, 'email', 'This email already registered')
+                default:
+                    setError(form, 'name', '')
+                    setError(form, 'email', '')
+                    setError(form, 'password', '')
+                    return setError(form, 'password_repeat', 'Unknown error')
+            }
+
+        }
+
+        redirect(302, '/login');
         return {
             form,
         };
