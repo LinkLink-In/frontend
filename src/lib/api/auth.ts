3c362fe0dc1af22b8c70b5a14a6edcd98d553cd3
Daniil Nikulin <d.nikulin@innopolis.university>
2024-06-30 16:10:38 +0300

    feat: add userRegister func

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -3,6 +3,15 @@ export interface BearerResponse extends Response {
     token_type: string;
     detail?: string;
 }
+export interface UserRead extends Response {
+    id: string;
+    email: string;
+    is_active?: boolean;
+    is_superuser?: boolean;
+    is_verified?: boolean;
+    name: string;
+    detail?: string;
+}
 export async function userLogin(email: string, password: string): Promise<BearerResponse> {
     return new Promise((resolve) => {
         fetch(`${import.meta.env.VITE_API_HOST}/auth/login`, {
@@ -14,6 +23,19 @@ export async function userLogin(email: string, password: string): Promise<Bearer
         }).then(
             (r) => r.json()
         ).then((data: BearerResponse) => {
+            resolve(data);
+        })
+    })
+}
+
+export async function userRegister(name: string, email: string, password: string): Promise<UserRead> {
+    return new Promise((resolve) => {
+        fetch(`${import.meta.env.VITE_API_HOST}/auth/register`, {
+            method: 'POST',
+            body: JSON.stringify({ email, password, name })
+        }).then(
+            (r) => r.json()
+        ).then((r) => r.json()).then((data: UserRead) => {
             resolve(data);
         })
     })
