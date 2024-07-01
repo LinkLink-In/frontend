d0fea4d5a90847582a8f428c7e801b0e9ba7bb6d
Daniil Nikulin <whatdanman@gmail.com>
2024-07-01 23:43:26 +0300

    feat: banner api support

---
+++ src/lib/api/banners.ts
@@ -0,0 +1,31 @@
+interface BannerCreate {
+    title: string;
+    description: string;
+}
+
+export interface BannerRead {
+    title: string;
+    description: string;
+    id: string;
+    owner_id: string;
+}
+export async function createBanner(data: BannerCreate, token: string): Promise<BannerRead> {
+    return new Promise((resolve) => {
+        fetch(`${import.meta.env.VITE_API_HOST}/banners/create`, {
+            method: 'POST',
+            headers:{
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(
+            (r) => {
+                return r.json()
+            }
+        ).then((data: BannerRead) => {
+            resolve(data);
+        }).catch((e) => {
+            console.log("banner", e);
+        })
+    })
+}
