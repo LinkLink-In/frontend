dccd0be9c7579d9625c7e2bf58287cef64ce9d27
Daniil Nikulin <whatdanman@gmail.com>
2024-07-01 23:44:02 +0300

    chore: adapt schema

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -1,13 +1,11 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    short_id: z.string(),
+    short_id: z.string().min(1),
     redirect_url: z.string().url('').startsWith("https://"),
-    expiration_date: z.string(),
-    redirects_limit: z.number(),
-    redirects_left: z.number(),
-    passphrase_hash: z.string(),
+    expiration_date: z.string().min(1),
+    redirects_limit: z.string(),
     banner_id: z.string()
-});
+}).required();
 
 export type FormSchema = typeof formSchema;
