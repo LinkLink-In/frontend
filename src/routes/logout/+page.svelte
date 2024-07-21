87efe934d67dc9100dc8eae4224a085bdf4ac9d9
Daniil Nikulin <whatdanman@gmail.com>
2024-07-22 01:06:20 +0300

    feat: add logout page

---
+++ src/routes/logout/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { loginStatus } from '$lib/stores/loginStatus';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	export let data;
+
+	onMount(() => {
+		if (data.success) {
+			goto('/');
+		}
+	});
+</script>
+
+<h1 class="flex min-h-full w-full items-center justify-center text-4xl text-[#1A1B23] opacity-50">
+	Logging out...
+</h1>
