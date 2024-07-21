4efcaaae3f2e145b25f567f78eb1b26d2306ae39
Daniil Nikulin <whatdanman@gmail.com>
2024-07-22 01:24:28 +0300

    chore: fix colors

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,8 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1 class="flex min-h-full w-full items-center justify-center text-4xl text-[#1A1B23] opacity-50">
+<h1
+	class="flex min-h-full w-full items-center justify-center text-4xl text-[#1A1B23] opacity-50 dark:text-[#FFFFFF]"
+>
 	{$page.error.message}
 </h1>
--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -7,11 +7,14 @@
 
 	onMount(() => {
 		if (data.success) {
+			loginStatus.set(false);
 			goto('/');
 		}
 	});
 </script>
 
-<h1 class="flex min-h-full w-full items-center justify-center text-4xl text-[#1A1B23] opacity-50">
+<h1
+	class="flex min-h-full w-full items-center justify-center text-4xl text-[#1A1B23] opacity-50 dark:text-[#FFFFFF]"
+>
 	Logging out...
 </h1>
